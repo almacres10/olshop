c75cef987ee41949b71bdeb2002d5822c2db87e3
Alvin Marco <alvinmarco8@gmail.com>
2023-01-16 22:16:54 +0700

    update

--- application/views/v_cekout.php
+++ application/views/v_cekout.php
@@ -1,135 +1,178 @@
 <!-- Main content -->
 <div class="invoice p-3 mb-3">
-              <!-- title row -->
-              <div class="row">
-                <div class="col-12">
-                  <h4>
-                    <i class="fas fa-globe"></i> AdminLTE, Inc.
-                    <small class="float-right">Date: 2/10/2014</small>
-                  </h4>
-                </div>
-                <!-- /.col -->
-              </div>
-              <!-- info row -->
-              <div class="row invoice-info">
-                <div class="col-sm-4 invoice-col">
-                  From
-                  <address>
-                    <strong>Admin, Inc.</strong><br>
-                    795 Folsom Ave, Suite 600<br>
-                    San Francisco, CA 94107<br>
-                    Phone: (804) 123-5432<br>
-                    Email: info@almasaeedstudio.com
-                  </address>
-                </div>
-                <!-- /.col -->
-                <div class="col-sm-4 invoice-col">
-                  To
-                  <address>
-                    <strong>John Doe</strong><br>
-                    795 Folsom Ave, Suite 600<br>
-                    San Francisco, CA 94107<br>
-                    Phone: (555) 539-1037<br>
-                    Email: john.doe@example.com
-                  </address>
-                </div>
-                <!-- /.col -->
-                <div class="col-sm-4 invoice-col">
-                  <b>Invoice #007612</b><br>
-                  <br>
-                  <b>Order ID:</b> 4F3S8J<br>
-                  <b>Payment Due:</b> 2/22/2014<br>
-                  <b>Account:</b> 968-34567
-                </div>
-                <!-- /.col -->
-              </div>
-              <!-- /.row -->
+  <!-- title row -->
+  <div class="row">
+    <div class="col-12">
+      <h4>
+        <i class="fas fa-shopping-cart"></i> Check Out
+        <small class="float-right">Date: 2/10/2014</small>
+      </h4>
+    </div>
+    <!-- /.col -->
+  </div>
+  <!-- info row -->
+  <div class="row invoice-info">
 
-              <!-- Table row -->
-              <div class="row">
-                <div class="col-12 table-responsive">
-                  <table class="table table-striped">
-                    <thead>
-                    <tr>
-                      <th>Qty</th>
-                      <th>Barang</th>
-                      <th>Harga</th>
-                      <th>Total Harga</th>
-                      <th>Berat</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($variable as $key => $value) { ?>
-                            <tr>
-                            <td>1</td>
-                            <td>Call of Duty</td>
-                            <td>455-981-221</td>
-                            <td>El snort testosterone trophy driving gloves handsome</td>
-                            <td>$64.50</td>
-                          </tr>
-                        <?php } ?>
-                    </tbody>
-                  </table>
-                </div>
-                <!-- /.col -->
-              </div>
-              <!-- /.row -->
+    <!-- /.col -->
+    <div class="col-sm-4 invoice-col">
 
-              <div class="row">
-                <!-- accepted payments column -->
-                <div class="col-6">
-                  <p class="lead">Payment Methods:</p>
-                  <img src="../../dist/img/credit/visa.png" alt="Visa">
-                  <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-                  <img src="../../dist/img/credit/american-express.png" alt="American Express">
-                  <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
+    </div>
+    <!-- /.col -->
+    <div class="col-sm-4 invoice-col">
+      <b>Invoice #007612</b><br>
+      <br>
+      <b>Order ID:</b> 4F3S8J<br>
+      <b>Payment Due:</b> 2/22/2014<br>
+      <b>Account:</b> 968-34567
+    </div>
+    <!-- /.col -->
+  </div>
+  <!-- /.row -->
 
-                  <p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">
-                    Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
-                    plugg
-                    dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
-                  </p>
-                </div>
-                <!-- /.col -->
-                <div class="col-6">
-                  <p class="lead">Amount Due 2/22/2014</p>
+  <!-- Table row -->
+  <div class="row">
+    <div class="col-12 table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Qty</th>
+            <th>Barang</th>
+            <th>Harga</th>
+            <th>Total Harga</th>
+            <th>Berat</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $i = 1;
+          $tot_berat = 0;
+          foreach ($this->cart->contents() as $items) {
+            $barang = $this->m_home->detail_barang($items['id']);
+            $berat = $items['qty'] * $barang->berat;
+            $tot_berat += $berat;
+          ?>
+            <tr>
+              <td>
+                <?php echo $items['qty']; ?>
+              </td>
+              <td>
+                <?php echo $items['name']; ?>
+              </td>
+              <td>Rp.<?php echo number_format($items['price'], 0); ?></td>
+              <td>Rp.<?php echo number_format($items['subtotal'], 0); ?></td>
+              <td><?= $berat ?> Gr </td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
+    <!-- /.col -->
+  </div>
+  <!-- /.row -->
 
-                  <div class="table-responsive">
-                    <table class="table">
-                      <tr>
-                        <th style="width:50%">Subtotal:</th>
-                        <td>$250.30</td>
-                      </tr>
-                      <tr>
-                        <th>Tax (9.3%)</th>
-                        <td>$10.34</td>
-                      </tr>
-                      <tr>
-                        <th>Shipping:</th>
-                        <td>$5.80</td>
-                      </tr>
-                      <tr>
-                        <th>Total:</th>
-                        <td>$265.24</td>
-                      </tr>
-                    </table>
-                  </div>
-                </div>
-                <!-- /.col -->
-              </div>
-              <!-- /.row -->
+  <div class="row">
+    <!-- accepted payments column -->
 
-              <!-- this row will not appear when printing -->
-              <div class="row no-print">
-                <div class="col-12">
-                  <a href="invoice-print.html" rel="noopener" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
-                  <button type="button" class="btn btn-success float-right"><i class="far fa-credit-card"></i> Submit
-                    Payment
-                  </button>
-                  <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
-                    <i class="fas fa-download"></i> Generate PDF
-                  </button>
-                </div>
-              </div>
-            </div>
-            <!-- /.invoice -->
+    <div class="col-sm-8 invoice-col">
+      Tujuan
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="form-group">
+            <label>Provinsi</label>
+            <select name="provinsi" class="form-control"></select>
+          </div>
+        </div>
+
+        <div class="col-sm-6">
+          <div class="form-group">
+            <label>Kota</label>
+            <select name="kota" class="form-control">
+            </select>
+          </div>
+        </div>
+
+        <div class="col-sm-6">
+          <div class="form-group">
+            <label>Ekspedisi</label>
+            <select name="ekspedisi" class="form-control">
+            </select>
+          </div>
+        </div>
+
+        <div class="col-sm-6">
+          <div class="form-group">
+            <label>Paket</label>
+            <select name="paket" class="form-control">
+            </select>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <!-- /.col -->
+    <div class="col-4">
+
+      <div class="table-responsive">
+        <table class="table">
+          <tr>
+            <th style="width:50%">Subtotal:</th>
+            <td>Rp.<?php echo $this->cart->format_number($this->cart->total()); ?></td>
+          </tr>
+          <tr>
+            <th>Berat:</th>
+            <td><?= $tot_berat ?></td>
+          </tr>
+          <tr>
+            <th>Ongkir</th>
+            <td><label>0</label></td>
+          </tr>
+          <tr>
+            <th>Total Bayar:</th>
+            <td><label>0</label></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <!-- /.col -->
+  </div>
+  <!-- /.row -->
+
+  <!-- this row will not appear when printing -->
+  <div class="row no-print">
+    <div class="col-12">
+      <a href="invoice-print.html" rel="noopener" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
+      <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
+        <i class="fas fa-shopping-cart"></i> Proses Cekout
+      </button>
+    </div>
+  </div>
+</div>
+<!-- /.invoice -->
+
+<script>
+  $(document).ready(function() {
+    //provinsi
+    $.ajax({
+      type: "POST",
+      url: "<?= base_url('rajaongkir/provinsi') ?>",
+      success: function(hasil_provinsi) {
+        //console.log(hasil_provinsi)
+        $("select[name=provinsi]").html(hasil_provinsi);
+      }
+    });
+
+    //kota
+    $("select[name=provinsi]").on("change", function() {
+      var id_provinsi_terpilih = $("option:selected", this).attr("id_provinsi");
+
+      $.ajax({
+        type: "POST",
+        url: "<?= base_url('rajaongkir/kota')  ?>",
+        data: 'id_provinsi=' + id_provinsi_terpilih,
+        success: function(hasil_kota) {
+          $("select[name=kota]").html(hasil_kota);
+        }
+      });
+    });
+  });
+</script>
